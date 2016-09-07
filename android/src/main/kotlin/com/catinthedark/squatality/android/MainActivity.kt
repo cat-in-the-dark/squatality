5ea93e9e8e7c04aadd219c5c174a08336b58c8ab
Ilya Siganov <ilya.blan4@gmail.com>
2016-09-07 22:37:02 +0600

    add libgdx

--- android/src/main/kotlin/com/catinthedark/squatality/android/MainActivity.kt
+++ android/src/main/kotlin/com/catinthedark/squatality/android/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.catinthedark.squatality.android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import com.catinthedark.squatality.SquatalityGame
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AndroidApplication() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val cfg = AndroidApplicationConfiguration()
+        initialize(SquatalityGame(), cfg)
     }
 }
---
+++ game/src/main/kotlin/com/catinthedark/squatality/SquatalityGame.kt
@@ -0,0 +1,32 @@
+package com.catinthedark.squatality
+
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class SquatalityGame : Game() {
+    lateinit var batch: SpriteBatch
+    lateinit var logo: Texture
+
+    override fun create() {
+        logo = Texture("textures/logo.png")
+        batch = SpriteBatch()
+    }
+
+    override fun render() {
+        super.render()
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch.begin()
+        batch.draw(logo, 0f, 0f)
+        batch.end()
+    }
+
+    override fun dispose() {
+        super.dispose()
+        batch.dispose()
+        logo.dispose()
+    }
+}
