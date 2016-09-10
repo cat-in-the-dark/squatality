b604b2dc5ec81e85f4610091146714422b03b8b8
Ilya Siganov <ilya.blan4@gmail.com>
2016-09-10 22:11:11 +0600

    add network control example

--- client/index.js
+++ client/index.js
@@ -3,6 +3,23 @@
 
   initConnection();
 
+  function State(sync) {
+    this.gameState = null;
+    this.moveMessage = null;
+    this.sync = sync;
+  }
+
+  State.prototype.onMove = function (speed, angle, stateName) {
+    if (this.moveMessage) {
+      this.moveMessage.speedX += speed.x;
+      this.moveMessage.speedY += speed.y;
+      this.moveMessage.angle += angle;
+      this.moveMessage.stateName = stateName;
+    } else {
+      this.moveMessage = {speedX: speed.x, speedY: speed.y, angle: angle, stateName: 'RUNNING'};
+    }
+  };
+
   function initConnection() {
     const eventName = 'message';
     const socket = io('http://localhost:8080');
@@ -30,7 +47,8 @@
       ThrowBrickMessage: 'com.catinthedark.models.ThrowBrickMessage'
     };
     const handler = {};
-    const state = {gameState: null};
+
+    let state = null;
     handler[Messages.ServerHelloMessage] = (data) => {
       console.log('ServerHelloMessage handler');
       const msg = {'className': Messages.HelloMessage, data: {name: 'Browser'}};
@@ -38,11 +56,17 @@
     };
 
     handler[Messages.GameStateMessage] = (data) => {
+      if (state == null) return;
       state.gameState = data.gameStateModel;
     };
 
     handler[Messages.GameStartedMessage] = (data) => {
       console.log('GameStartedMessage handler');
+      state = new State(() => {
+        const msg = {'className': Messages.MoveMessage, data: state.moveMessage};
+        socket.emit(eventName, JSON.stringify(msg));
+        state.moveMessage = null;
+      });
       initGame(state);
     };
   }
@@ -51,21 +75,57 @@
   * Yes. We connect network and game by state object. It's mutable!
   **/
   function initGame(state) {
-    const game = new Phaser.Game(1081, 652, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    const game = new Phaser.Game(1081, 652, Phaser.WEBGL, '', { preload: preload, create: create, update: update });
+    let syncTimer = 0;
     function preload() {
       game.load.spritesheet('gop', 'gop_blue.png', 108, 108, 19);
     }
 
-    var sprite = null;
+    function releaseUnit(x, y) {
+      const sprite = game.add.sprite(x, y, 'gop');
+      sprite.animations.add('run', [0, 1, 2, 3, 4, 5, 6]);
+      sprite.animations.add('idle', [0]);
+      sprite.animations.play('idle', 10, true);
+      return sprite;
+    }
+
+    let player = null;
     function create() {
-      sprite = game.add.sprite(0, 0, 'gop');
-      sprite.animations.add('walk', [0, 1, 2, 3, 4, 5, 6]);
-      sprite.animations.play('walk', 10, true);
+      player = releaseUnit(0,0);
     }
 
     function update() {
-      sprite.x = state.gameState.me.x;
-      sprite.y = state.gameState.me.y;
+
+      let speed = {x: 0.0, y: 0.0};
+      let stateName = 'IDLE';
+      if (game.input.keyboard.isDown(Phaser.Keyboard.A)) {
+        speed.x -= 5.0;
+      }
+      if (game.input.keyboard.isDown(Phaser.Keyboard.D)) {
+        speed.x += 5.0;
+      }
+      if (game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+        speed.y -= 5.0;
+      }
+      if (game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+        speed.y += 5.0;
+      }
+      if (speed.x != 0.0 || speed.y != 0) stateName = 'RUNNING';
+      state.onMove(speed, 0, stateName);
+
+      syncTimer += this.time.elapsedMS;
+      if (syncTimer > 40) {
+        state.sync(syncTimer);
+        syncTimer = 0;
+      }
+      player.x = state.gameState.me.x;
+      player.y = state.gameState.me.y;
+      player.angle = state.gameState.me.angle;
+      if (state.gameState.me.state == 'RUNNING') {
+        player.animations.play('run', 10, true);
+      } else {
+        player.animations.play('idle', 10, true);
+      }
     }
   }
 })();
