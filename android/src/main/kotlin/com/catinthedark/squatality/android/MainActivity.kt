629c2c6a63a0c12ed81260a507d53f8c8fb8eae4
Ilya Siganov <ilya.blan4@gmail.com>
2016-10-04 22:42:05 +0600

    rm strict mode

--- android/src/main/kotlin/com/catinthedark/squatality/android/MainActivity.kt
+++ android/src/main/kotlin/com/catinthedark/squatality/android/MainActivity.kt
@@ -9,19 +9,6 @@ import com.catinthedark.squatality.game.SquatalityGame
 class MainActivity : AndroidApplication() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        StrictMode
-            .setThreadPolicy(StrictMode.ThreadPolicy.Builder()
-                .detectCustomSlowCalls()
-                .detectNetwork()
-                .penaltyLog()
-                .build())
-        StrictMode
-            .setVmPolicy(StrictMode.VmPolicy.Builder()
-                .detectCleartextNetwork()
-                .detectLeakedClosableObjects()
-                .penaltyLog()
-                .penaltyDeath()
-                .build())
         super.onCreate(savedInstanceState)
         val cfg = AndroidApplicationConfiguration().apply {
             useImmersiveMode = true
